225eb3c21750f7344fcecbc5b029338f503b55c7
Matthew <matthewmcadam9@gmail.com>
2025-04-03 12:38:48 -0400

    More rework

--- runme.py
+++ runme.py
@@ -3,7 +3,7 @@ import pybullet_data
 import time
 import numpy as np
 import math as m
-from motors import motor
+from sub_sim.motor import motor
 
 # Constants
 WATER_DENSITY = 1000  # kg/m^3 (density of water)
